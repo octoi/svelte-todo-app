29061c2990b489846c47338625ad8467d5f59962
fadhilsaheer <fadhilsaheer@gmail.com>
2022-01-10 20:49:29 +0530

    todo routes

---
+++ api/src/routes/todo.ts
@@ -0,0 +1,55 @@
+import { Router } from 'express';
+import {
+  getAllTodos,
+  createTodo,
+  resolveTodo,
+  deleteTodo,
+} from '../mongo/helpers';
+
+export const router = Router();
+
+router.get('/', (req, res) => {
+  getAllTodos()
+    .then((data) => res.json(data).status(200))
+    .catch((err) => res.send(err).status(404));
+});
+
+router.post('/create', (req, res) => {
+  const todo = req.body?.todo;
+
+  if (!todo) {
+    res.send('required field not found').status(400);
+    return;
+  }
+
+  createTodo(todo)
+    .then((data) => res.json(data).status(200))
+    .catch((err) => res.send(err).status(404));
+});
+
+router.put('/resolve/:id', (req, res) => {
+  const todoId = req.params.id;
+  const resolve = req.body?.resolve;
+
+  if (!todoId || !resolve) {
+    res.send('required field not found').status(400);
+    return;
+  }
+
+  resolveTodo(todoId, resolve)
+    .then((data) => res.json(data).status(200))
+    .catch((err) => res.send(err).status(404));
+});
+
+router.delete('/delete/:id', (req, res) => {
+  const todoId = req.params.id;
+
+  if (!todoId) {
+    res.send('required field not found').status(400);
+    return;
+  }
+
+  deleteTodo(todoId)
+    .then((data) => res.json(data).status(200))
+    .catch((err) => res.send(err).status(404));
+});
