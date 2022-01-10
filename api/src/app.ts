183bccb38f69479a7d3095882dc02ffcabb668a4
fadhilsaheer <fadhilsaheer@gmail.com>
2022-01-10 20:27:37 +0530

    app file

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,1 +1,14 @@
-console.log('Hello World');
+import express from 'express';
+import { connectDB } from './mongo';
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`[+] SERVER STARTED AT PORT ${PORT} 🚀`);
+  console.log(`[i] http://localhost:${PORT}`);
+  connectDB();
+});
---
+++ api/src/mongo/helpers.ts
@@ -0,0 +1,1 @@
+import { TodoModel } from './TodoSchema';
