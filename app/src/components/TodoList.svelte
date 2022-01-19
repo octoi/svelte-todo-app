97e85690a459bb570a16a189ed5e3753d689a7ab
fadhilsaheer <fadhilsaheer@gmail.com>
2022-01-19 16:16:13 +0530

    Get todos

---
+++ app/src/components/TodoList.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import { onMount } from 'svelte';
+  import { getTodos } from '../api';
+  import { todoStore } from '../store/todoStore';
+
+  onMount(() => {
+    getTodos().then((todos: any) => todoStore.set(todos));
+  });
+</script>
+
+<div class="mt-5">
+  <!-- todo details -->
+</div>
