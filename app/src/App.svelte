8ada24428bfcabd7f65922de0110eb5d9af03e9a
fadhilsaheer <fadhilsaheer@gmail.com>
2022-01-19 07:51:29 +0530

    Todo Input

--- app/src/App.svelte
+++ app/src/App.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
+  import TodoInput from './components/TodoInput.svelte';
 </script>
 
-<main>
-  <h1 class="text-2xl font-bold font-sans underline decoration-gray-800">
-    Hello Typescript!
-  </h1>
+<main class="flex flex-col items-center mt-28">
+  <h1 class="font-bold text-3xl">Svelte Todo</h1>
+  <TodoInput />
 </main>
 
 <style>
---
+++ app/src/components/TodoInput.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  const handleFormSubmit = () => {
+    alert('Form submit');
+  };
+</script>
+
+<form
+  on:submit|preventDefault={handleFormSubmit}
+  class="mt-5 w-full flex justify-center"
+>
+  <input
+    type="text"
+    placeholder="New Todo"
+    class="bg-app-secondaryDark p-4 rounded-md text-xl w-full md:w-1/3 border-2 border-app-secondaryDark outline-none focus:border-app-wisteria"
+  />
+  <button
+    class="font-medium text-xl ml-2 rounded-md px-3 bg-app-secondaryDark wrounded-full flex items-center justify-center transition-all hover:bg-app-wisteria hover:text-app-primaryDark"
+    >Add
+  </button>
+</form>
